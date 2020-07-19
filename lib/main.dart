90988b9829eccce4ffa4b03f81cd18d32ed8e34b
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-19 16:44:06 +0530

    Adding some dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
+        // the app on. For desktop platforms, the controls will be smaller and 
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
